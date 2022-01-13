0a6e646ce4009ca34ad4e545b1b21bb60c09bd82
Frithjof Klös <frithjof.kloes@shopgate.com>
2022-01-13 12:11:03 +0100

    CCP-2521 Increased z-index of component to avoid visible gallery pagination floating above it on pdp

--- frontend/components/ScrollHeader/style.js
+++ frontend/components/ScrollHeader/style.js
@@ -4,7 +4,7 @@ export const header = css({
   position: 'sticky',
   top: 0,
   left: 0,
-  zIndex: 2,
+  zIndex: 100,
   transform: 'translateY(0)',
   transition: 'transform 0.3s ease',
 }).toString();
